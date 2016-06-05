c573d5c77330f4f150172c7a2ab43aa1d8988fdb
Leonard Siebeneicher <leonard7e@gmail.com>
2016-06-05 21:17:08 +0200

    Das Dropdown-Menü geschlossen lassen, falls der Benutzer JavaScript ausgeschaltet hat

---
+++ js/directives/requireJs.js
@@ -0,0 +1,10 @@
+
+app.directive('requireJs', function() {
+  return {
+    'restrict': 'A',
+    link: function($scope, $element, $attributes) {
+      $element.removeClass("RequireJS");
+      console.log("->");
+    }
+  };
+});
