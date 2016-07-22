614a76bdbd82cc8ea08cc2928f01e7fc43ee4ed3
Leonard Siebeneicher <leonard7e@googlemail.com>
2016-07-22 13:32:51 +0200

    Switching to ng-class to achieve dropdown menu functionality.

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -20,7 +20,7 @@ app.controller('Banner', function ($scope, $window) {
   $scope.toggleSandwich = function() {
     $scope.sandwichMenuOpened = !$scope.sandwichMenuOpened;
   };
-  
+
   var win = angular.element($window);
   win.bind('resize', function () {
     $scope.$apply( query_media )
@@ -28,57 +28,19 @@ app.controller('Banner', function ($scope, $window) {
 });
 
 app.controller('DropdownNav', function ($scope, $document) {
-  $scope.$on('menuItemEvent', function (event, msg) {
-    if (msg.type === "enterMenu") {
-      $scope.$broadcast('popupCtlEvent', {
-        type: 'show',
-        navId: msg.navId } );
-    } else if (msg.type === "itemBlur") {
-      $scope.$broadcast('popupCtlEvent', {
-        type: 'itemBlur',
-        navId: msg.navId } );
-    }
-  });
-});
+  $scope.activeMenu = -1;
 
-app.controller('Dropdown', function ($scope, $document) {
-  $scope.itsId = null;
-  $scope.doPopup = false;
-  $scope.itemBlur = false;
 
-  $scope.init = function (target) {
-    $scope.itsId = target;
-    $document.bind("click", function (event) {
-      $scope.$apply(function(){
-        $scope.doPopup = false;
-      });
-    });
+  $scope.doDisable = function () {
+    $scope.activeMenu = -1;
+  }
+  $scope.doActivate = function(menuid) {
+    $scope.activeMenu = menuid;
+    event.stopPropagation();
+  };
+  $scope.isActive = function(menuid) {
+    return $scope.activeMenu === menuid;
   };
 
-  $scope.$on('popupCtlEvent', function (event, msg) {
-    if (msg.navId === $scope.itsId || msg.navId == "_ALL_") {
-      if        ( msg.type === 'show') {
-        $scope.doPopup = true;
-        $scope.itemBlur = false;
-      } else if ( msg.type === 'hide') {
-        $scope.doPopup = false;
-        $scope.itemBlur = false;
-      } else if ( msg.type === 'itemBlur') {
-        $scope.itemBlur = true;
-        setTimeout(function(){
-          // Wait some time
-          if ($scope.itemBlur) {
-            $scope.doPopup=false;
-            $scope.itemBlur=false;
-            $scope.$digest();
-          }
-        }, 100);
-      }
-    }
-    else if (msg.type == "show") {
-      // Hide, because other popups are shown
-      $scope.doPopup = false;
-    }
-    $scope.$digest();
-  });
+  $document.on('click', function (){$scope.$apply($scope.doDisable)});
 });
