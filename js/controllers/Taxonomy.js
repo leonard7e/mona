41cf9ec8e4c6dfb5ccb430711531aacfda5c0df5
Leonard Siebeneicher <leonard7e@googlemail.com>
2016-08-15 11:37:58 +0200

    Responsive Design: get taxonomy switches working for mobile phones

--- js/controllers/Taxonomy.js
+++ js/controllers/Taxonomy.js
@@ -110,3 +110,23 @@ app.controller('taxonomy', function($scope){
     return tax_match(tax, name, false);
   };
 });
+
+
+app.controller('sidebarCtrl', function ($scope) {
+  $scope.filterActive = [];
+  $scope.filterInit = function (idx) {
+    $scope.filterActive.push(idx);
+    // insert idx into filterActive only once
+  };
+  $scope.filterSwitch = function (idx) {
+    var pos = $scope.filterActive.indexOf(idx);
+    if (pos >= 0) {
+      $scope.filterActive.splice(pos, 1);
+    } else {
+      $scope.filterActive.push(idx);
+    }
+  };
+  $scope.filterIsActive = function (idx) {
+    return ($scope.filterActive.indexOf(idx) === -1);
+  };
+});
