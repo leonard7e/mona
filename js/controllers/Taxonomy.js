7ed3406d1bd7039c852f9ec35ed4e77a9335e62c
Leonard Siebeneicher <leonard7e@googlemail.com>
2016-09-01 16:12:47 +0200

    Reworking Taxonomy
    Taxonomy supports now "Town".
    Taxonomy switches are hidden automatically.

--- js/controllers/Taxonomy.js
+++ js/controllers/Taxonomy.js
@@ -1,36 +1,47 @@
 app.controller('taxonomy', function($scope){
   // The selection of taxonomy items
   $scope.taxSelect = {
+    "town": [],
     "category": [],
-    "tag": []
+    "tag": [],
   };
 
   // Complete taxonomy hierarchy
   $scope.taxonomy = {
+    "town": [],
     "category": [],
-    "tag": []
+    "tag": [],
   };
 
   $scope.taxInit = function (tax) {
-    function register_tax (category, tag) {
-      function add_if_needed(name, target, value) {
-        if ( $scope.taxonomy[name][target] === undefined ){
-          $scope.taxonomy[name][target] = [value];
-        } else if ( !($scope.taxonomy[name][target].indexOf(value) >= 0) ){
-          $scope.taxonomy[name][target].push(value);
-          $scope.taxonomy[name][target].sort(); // TODO: faster insert, basing on splice
+    function register_tax (town, category, tag) {
+      function add_if_needed(name, target, section, value) {
+        if ( $scope.taxonomy[name][target] === undefined) {
+          $scope.taxonomy[name][target] = {};
+        } if ( $scope.taxonomy[name][target][section] === undefined ){
+          $scope.taxonomy[name][target][section] = [value];
+        } else if ( !($scope.taxonomy[name][target][section].indexOf(value) >= 0) ){
+          $scope.taxonomy[name][target][section].push(value);
+          $scope.taxonomy[name][target][section].sort(); // TODO: faster insert, basing on splice
         }
       };
-      add_if_needed("category", category, tag);
-      add_if_needed("tag", tag, category);
+      add_if_needed("town", town, "category", category);
+      add_if_needed("town", town, "tag", tag);
+      add_if_needed("category", category, "town", town);
+      add_if_needed("category", category, "tag", tag);
+      add_if_needed("tag", tag, "town", town);
+      add_if_needed("tag", tag, "category", category);
     };
 
-    tax.category.forEach(function(category) {
-      tax.tag.forEach(function(tag) {
-        register_tax(category, tag);
+    tax.town.forEach(function(town) {
+      tax.category.forEach(function(category) {
+        tax.tag.forEach(function(tag) {
+          register_tax(town, category, tag);
+        })
       })
     });
   };
+
 
   // ---
   function tax_match ( tax, name, def ) {
@@ -49,32 +60,54 @@ app.controller('taxonomy', function($scope){
     return def;
   }
 
+
   // ---
   $scope.taxHideBlogItem = function (tax) {
-    return !(tax_match(tax, "category", true) && tax_match(tax, "tag", true));
+    return !(tax_match(tax, "town", true) && tax_match(tax, "category", true) && tax_match(tax, "tag", true));
   };
 
+  $scope.taxSwitchActive = function (name, tax) {
+    return tax_match(tax, name, false);
+  };
+
+
+
+
   // ---
-  function hide_switch (name, target, value) {
-    if ($scope.taxSelect[target].length > 0) {
-      for ( i in $scope.taxSelect[target]) {
-        if ($scope.taxonomy[name][value].indexOf($scope.taxSelect[target][i]) >= 0) {
-          return false;
-        }
-      };
-      return true;
-    } else {
-      return false;
+  function hide_switch (name, value) {
+    var to_hide = undefined;
+    if (name == "town") {to_hide = ["category", "tag"]};
+    if (name == "category") {to_hide = ["town", "tag"]};
+    if (name == "tag") {to_hide = ["town","category"]};
+
+    for (x = 0; x<to_hide.length; ++x) {
+      var target = to_hide[x];
+      if ($scope.taxSelect[target].length > 0) {
+        for ( i in $scope.taxSelect[target]) {
+          if($scope.taxonomy[name][value][target] == undefined)
+          {
+            console.log(name, target, $scope.taxonomy[name][value])
+          };
+          if ($scope.taxonomy[name][value][target].indexOf($scope.taxSelect[target][i]) >= 0) {
+            return false;
+          }
+        };
+        return true;
+      }
     }
+    return false;
   };
 
-  // ---
+  $scope.taxHideTownSwitch = function (tw) {
+    return hide_switch("town", tw);
+  };
+
   $scope.taxHideCategorySwitch = function (c) {
-    return hide_switch("category", "tag", c);
+    return hide_switch("category", c);
   };
 
   $scope.taxHideTagSwitch = function (t) {
-     return hide_switch("tag", "category", t);
+     return hide_switch("tag", t);
   };
 
   // ---
@@ -104,10 +137,6 @@ app.controller('taxonomy', function($scope){
     };
 
     switch_tax(name,value);
-  };
-
-  $scope.taxSwitchActive = function (name, tax) {
-    return tax_match(tax, name, false);
   };
 });
 
